0a4ec1ad4863d43659ba346a87c5aef2cdf3c07b
lavet13 <lavet13@mail.ru>
2022-08-14 19:39:16 +0300

    high level of abstraction explanation

--- script.js
+++ script.js
@@ -1,8 +1,69 @@
 'use strict';
+
+/*
+// own example
+const addElementToTheEnd = function (array, element) {
+  return array.push(element);
+};
+const deleteLastElement = function (array) {
+  return array.pop(); // deleted element
+};
+
+const processingWithData = function (array, fn, element = -1) {
+  if (element !== -1) fn(array, element);
+  else fn(array);
+  console.log(`array with length of ${array.length}`);
+  console.log(array);
+};
+
+processingWithData([1, 2, 3, 4], addElementToTheEnd, 228);
+processingWithData([5, 6, 7, 8], deleteLastElement);
+
+
+const oneWord = function (str) {
+  return str.replace(/\s+/g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function(because it operates at a higher-level of abstraction, leaving the low level details to low level functions)
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`); // property of the function, so basically it's behave like all the other "type" of objects, such as arrays, objects, maps, sets etc.
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+// high5 function is the more lower level of abstraction
+const high5 = function () {
+  console.log('✋');
+};
+
+// addEventListener is the higher order function with the high level of abstraction
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+// so big advantage of callback functions is that it makes it easy to split up our code into more reusable and interconnected parts, where all the functionality is nicely split up into their own functions and that itself is really helpful, but there is a second and way more important advantage which is the fact that callback functions allow us to create abstraction, so basically what abstraction means is that we hide the detail of some code implementation because we don't really care about all that detail, and this allows us to think about problems at a higher more abstract level, the transformer function is really only concerned with transforming the input string itself. But no matter how that transforming itself actually works. So it's basically delegating the string transformation to the other lower level of functions, which are these two.
+*/
+
+/*
+/////////////////////////////////////////
+// First-Class and Higher-Order Functions
+// first-class functions is just a feature, that a programming language either has or does not have, All it means is that all functions are values, there are no first-class functions in practice, it's just a concept, there are however higher order functions in practice, which are possible because the language supports first-class functions. So it's a subtle difference.
+*/
+
+/*
 /////////////////////////////////////////
 // How Passing Arguments Works_ Value vs. Reference
 
 const flight = 'LH234';
+
 const jonas = {
   name: 'Jonas Schmedtmann',
   passport: 238974293874,
@@ -32,6 +93,7 @@ const newPassport = function (person) {
 newPassport(jonas);
 checkIn(flight, jonas); // we are passing a reference to the function, but not by reference - that's an important distinction :-D
 console.log({ flight, jonas });
+*/
 
 /*
 /////////////////////////////////////////
@@ -44,7 +106,7 @@ const createBooking = function (
   price = 199 * numPassengers
 ) {
   // ES5
-  // numPassengers ||= 1; // catch 0, "", null, undefined etc. falsy values, and assign default value of 1 to a variable
+  // numPassengers ||= 1; // catch 0, "", null, undefined etc., so it goes to the second operand to assign a default value
   // price ||= 199;
 
   const booking = {
