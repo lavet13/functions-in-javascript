c2a6b3ff6f3924e3be66b8f7f86451d3b1ecec0f
lavet13 <lavet13@mail.ru>
2022-08-18 19:00:34 +0300

    closures and examples of that

--- script.js
+++ script.js
@@ -2,9 +2,122 @@
 
 /*
 //////////////////////////////////////////////////////////////
+// More Closure Examples
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+// the variable environment of g is no longer there, but f function closed over that variable environment and therefore it is able to access the "a" variable
+g();
+f();
+
+console.dir(f);
+
+// Reassigning f function by h function
+h();
+f();
+
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3; // closure has priority over scope chain which is obvisous
+
+  // the callback function was able to use all the variables that were in the variable environment in which it(callback function) was created, this is a clear sign of a closure being created;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+*/
+
+/*
+//////////////////////////////////////////////////////////////
+// Closures
+
+// we don't create closures manually, we need to recognize those situations;
+// the environment in which the "secureBooking" function was created is no longer active, it is in fact gone, but still the booker function somehow continues to have access to the variables that were present at the time that the function was created, and in particular passengerCount variable, so that's exactly what the closure does, it makes a function remember all the variables that existed at the function's birthplace essentially, we can imagine that secure booking as being the birthplace of the function that is below, so of the booker function, function remembers everything at it's birthplace, by the time it was created, and this cannot simply be explained with the scope chain alone;
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+// any function always has access to the variable environment of the execution context in which the function was created, now in the case of booker, this function was created it was born in the execution context of secureBooking function which was popped off the stack previously, booker function will get access to this variable environment which contains the passengerCount variable, and this is how the function will be able to read and manipulate the passengerCount variable, the closure is basically the variable environment attached to a function exactly as it was at the time and place that the function was created, so in a sense, the scope chain is actually preserved through the closure, even when a scope has already been destroyed, because it's execution context is gone, this means that even though the execution context has actually been destoyed, the variable environment somehow keeps living somewhere in the engine, now we can say that the booker function closed over it's parent scope or over it's parent variable environment and this include all function arguments, even though in this example, we don't have any, and now this attached or closed over variable environment stays with the function forever, it will carry it around and be able to use it forever, to make it a bit more digestible(усваиваемый) we can also say that thanks to the closure a function does not lose connection to variables that existed at the function's birthplace, that's a bit more intuitive;
+
+booker(); // function attempts to increase the passengerCount variable, however this variable is not in the current scope, and so JavaScript will immediately look into the closure, and see if it can find the variable there, and it does this even before looking at the scope chain, for example, if there was a global passengerCount variable set to 10, it would still first use the one in the closure, so the closure basically has priority over the scope chain, and so after running this function, the passengerCount becomes one, this message is logged and then the execution context is popped off the stack, then execution moves to the next line, we get a new execution context and a closure is still there, still attached to the function and the value is still one and so now this function executes, increasing the passengerCount to two, and logging a message again;
+booker();
+booker();
+
+console.dir(booker);
+*/
+
+/*
+//////////////////////////////////////////////////////////////
+// Immediately Invoked Function Expressions (IIFE)
+
+// regular function call
+const runOnce = function () {
+  console.log('This will never run again');
+};
+
+runOnce();
+
+// many times we need to protect our variables from being accidentally overwritten by some other parts of the program, or even with external scripts or libraries
+// IIFE (Immediately Invoked Function Expression)
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+// Arrow function IIAF??
+(() => console.log('This will ALSO never run again'))();
+
+// Block-scope ES6
+// IIFE are not that used anymore because if all we want is to create a new scope for data privacy, all we need to do is to just create a block, there's no need to creating a function to create a new scope, unless we want to use var for our variables, but we already know, we probably shouldn't do that, now on the other hand, if what you really need, is to execute a function just once, then the IIFE pattern is still the way to go, even now with modern JavaScript.
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+
+// console.log(isPrivate);
+console.log(notPrivate);
+*/
+
+/*
+//////////////////////////////////////////////////////////////
 // The call and apply Methods
 
-// we cannot use arrow function by using call or apply methods, it simply assign the "this" keyword to the window object
+// SOLVE we cannot use arrow function by using call, apply methods, it simply assign the "this" keyword to the window object, it's like override it
+
 const book = function (flightNum, name) {
   console.log(this); // initially the "this" keyword is undefined(in strict mode)
 
@@ -31,7 +144,7 @@ const eurowings = {
   bookings: [],
 };
 
-// Does not work with the "this" properly, so we might to set the "this" keyword manually by using bind method to the function itself
+// Does not work with the "this" properly, so we might to set the "this" keyword manually by using call, apply or bind method to the function itself
 // book(23, 'Sarah Williams');
 
 // Call Method
